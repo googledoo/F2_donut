e546a3a01bddfa0cd7adb93c6e45951b57e33d14
googledoo <dogdog.1@hotmail.com>
2016-04-30 14:36:58 +0700

    #06 ยานชนเส้นแล้วตาย

--- spw/GameEngine.java
+++ spw/GameEngine.java
@@ -58,8 +58,20 @@ public class GameEngine implements KeyListener{
  		
  	}
 		gp.updateGameUI();
+		Rectangle2D.Double vr = v.getRectangle();
+		Rectangle2D.Double er;
+		for(Enemy e : enemies){
+			er = e.getRectangle();
+			if(er.intersects(vr)){
+				die();
+				return;
+			}
+		}
 	}
 
+	public void die(){
+		timer.stop();
+	}
 	void controlVehicle(KeyEvent e) {
 		switch (e.getKeyCode()) {
 		case KeyEvent.VK_LEFT:
